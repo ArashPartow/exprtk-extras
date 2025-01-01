2bc34f93a2a119aa942f6eec92cb490b6502c70b
ArashPartow <partow@gmail.com>
2025-01-01 00:00:00 +0000

    C++ Mathematical Expression Library (ExprTk)  https://www.partow.net/programming/exprtk/index.html

--- exprtk_loop_timeout_rtc.cpp
+++ exprtk_loop_timeout_rtc.cpp
@@ -3,7 +3,7 @@
  *         C++ Mathematical Expression Toolkit Library        *
  *                                                            *
  * ExprTk Loop Timeout RTC                                    *
- * Author: Arash Partow (1999-2024)                           *
+ * Author: Arash Partow (1999-2025)                           *
  * URL: https://www.partow.net/programming/exprtk/index.html  *
  *                                                            *
  * Copyright notice:                                          *
--- exprtk_miller_rabin_primality_test.cpp
+++ exprtk_miller_rabin_primality_test.cpp
@@ -3,7 +3,7 @@
  *         C++ Mathematical Expression Toolkit Library        *
  *                                                            *
  * ExprTk Miller-Rabin Probabilistic Primality Test           *
- * Author: Arash Partow (1999-2024)                           *
+ * Author: Arash Partow (1999-2025)                           *
  * URL: https://www.partow.net/programming/exprtk/index.html  *
  *                                                            *
  * Copyright notice:                                          *
